589a51b3865be0e867d05df947fb29b3b1ae3872
robynainsley21 <robynainsley2i@gmail.com>
2024-05-17 08:58:34 +0200

    Complete exercise

---
+++ script/script.js
@@ -0,0 +1,23 @@
+//question 1
+const person1 = {
+    firstName: 'Robyn',
+    lastName: 'Carnow',
+    age: 23
+}
+
+//question 2
+const person2 = {
+    firstName: 'Nicki',
+    lastName: 'Abels',
+    age: 25
+}
+
+//question 3
+console.log(typeof person1);
+console.log(typeof person2);
+
+//question 4
+let personArray = [person1, person2]
+
+//question 5
+console.log(typeof personArray);
